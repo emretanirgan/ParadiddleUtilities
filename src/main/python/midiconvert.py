14f9d03d4d2502af940637eed05d10fe00bf2cce
Emre Tanirgan <tanirganemre@gmail.com>
2019-09-09 21:01:57 -0700

    UI Updates, some refactoring

--- src/main/python/midiconvert.py
+++ src/main/python/midiconvert.py
@@ -8,10 +8,6 @@ from PyQt5 import QtWidgets
 from ParadiddleUtilities import *
 import sys
 
-class MidiConverter():
-    def __init__(self):
-        print("Created")
-
 out_dict = {}  
 # TODO set up GUI for input midi, input drum set, output recording file names with file dialogs for each
 
@@ -19,19 +15,10 @@ out_dict = {}
 # If this file is not fed in, then use default
 # drum_set_file = '/Users/etanirgan/test.json'
 drum_set_file = ''
+drum_set_dict = None
+midi_file_name = ''
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-default_set_name = "../resources/base/drum_sets/defaultset.rlrr"
-default_set_full_path = os.path.join(script_dir, default_set_name)
-print(default_set_full_path)
-
-if drum_set_file == '':
-    drum_set_file = default_set_full_path
-drum_set_dict = None
-
-with open(drum_set_file) as f:
-    drum_set_dict = json.load(f)
-    print(len(drum_set_dict["DrumLayout"]))
 
 # TODO put in actual default notes
 # TODO support for drum types with more than 1 hit zone - can map from midi note to
@@ -78,86 +65,104 @@ rhythm_midi_note_to_drums = {
 
 note_to_drums_map = {}
 
-out_dict["DrumHits"] = []
-midi_file_name = script_dir + '/../resources/base/midi_files/notes.mid'
-mid = MidiFile(midi_file_name)
+def analyze_drum_set(drum_set_filename):
+    default_set_name = "../resources/base/drum_sets/defaultset.rlrr"
+    default_set_full_path = os.path.join(script_dir, default_set_name)
+    print(default_set_full_path)
 
-tempo = 500000
-#list of tuples in the form of (total_ticks, new_tempo)
-tempo_events = [(0.0, tempo)]
-total_time = 0.0
-total_ticks = 0.0
-longest_time = 0.0
+    if drum_set_filename == '':
+        drum_set_filename = default_set_full_path
+    drum_set_dict = None
 
-#TODO Pick track from a GUI (file dialog) to convert to rlrr
-#TODO need default midi mappings for rhythm game midi format - get difficulties, map from those midi notes
-#https://rockband.scorehero.com/forum/viewtopic.php?t=1711
-#https://www.scorehero.com/forum/viewtopic.php?t=1179
-#TODO convert from .chart? eventually
-#TODO don't hard code midi and rlrr file paths
+    with open(drum_set_filename) as f:
+        drum_set_dict = json.load(f)
+        print(len(drum_set_dict["DrumLayout"]))
 
-is_rhythm_game_midi = False
-class_to_default_notes = rlrr_default_notes
-track_to_convert = mid.tracks[0]
+    
 
-for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
-    if (track.name == "PART DRUMS"):
-        is_rhythm_game_midi = True
-        class_to_default_notes = rhythm_midi_note_to_drums
-        track_to_convert = track
+def analyze_midi_file():
+    out_dict["DrumHits"] = []
+    midi_file_name = script_dir + '/../resources/base/midi_files/notes.mid'
+    mid = MidiFile(midi_file_name)
 
-if drum_set_dict is None:
-    for drum_class in class_to_default_notes:
-        print(drum_class)
-        note_to_drums_map[class_to_default_notes[drum_class]]= [str(drum_class)+"Default"]
-else:
-    for drum_obj in drum_set_dict["DrumLayout"]:
-        if drum_obj["Class"] in class_to_default_notes:
-            note_to_drums_map[class_to_default_notes[drum_obj["Class"]]] = [str(drum_obj["DrumName"])]
-    out_dict["DrumLayout"] = drum_set_dict["DrumLayout"] 
+    tempo = 500000
+    #list of tuples in the form of (total_ticks, new_tempo)
+    tempo_events = [(0.0, tempo)]
+    total_time = 0.0
+    total_ticks = 0.0
+    longest_time = 0.0
 
-print(note_to_drums_map)
-tempo_total_ticks = 0
-tempo_index = 0
+    #TODO Pick track from a GUI (file dialog) to convert to rlrr
+    #TODO need default midi mappings for rhythm game midi format - get difficulties, map from those midi notes
+    #https://rockband.scorehero.com/forum/viewtopic.php?t=1711
+    #https://www.scorehero.com/forum/viewtopic.php?t=1179
+    #TODO convert from .chart? eventually
+    #TODO don't hard code midi and rlrr file paths
 
-for i, track in enumerate(mid.tracks): 
-    for msg in track:
-        if msg.is_meta:
-            if msg.type == "set_tempo":
-                tempo = msg.tempo
-                tempo_total_ticks += msg.time
-                tempo_events.append((tempo_total_ticks, msg.tempo))
+    is_rhythm_game_midi = False
+    class_to_default_notes = rlrr_default_notes
+    track_to_convert = mid.tracks[0]
 
-print("Tempo Changes: " + str(tempo_events))
-for msg in track_to_convert:
-    total_ticks += msg.time
-    while (tempo_index+1 < len(tempo_events)) and (total_ticks > tempo_events[tempo_index+1][0]):
-        tempo_index += 1
-    tempo = tempo_events[tempo_index][1]
-    # total_time = total_ticks * (tempo / (1000000.0 * mid.ticks_per_beat))
-    total_time = total_time + mido.tick2second(msg.time, mid.ticks_per_beat, tempo)
-    if(total_time > longest_time):
-        longest_time = total_time
-    if not msg.is_meta:
-        if msg.type == "note_on":
-            drum_name = "Test"
-            note = msg.note
-            # if is_rhythm_game_midi:
-                # note = msg.note 
-            #ignore velocity 0 notes here? Seem to be getting a lot of these in the rhythm game midis, almost like note off events are showing up here
-            if note in note_to_drums_map and msg.velocity > 0:
-                drum_name = note_to_drums_map[note][0]
-                drum_hit = {"DrumName" : drum_name, "Vel" : msg.velocity, "Loc": 0, "Time": '%.4f'%total_time}
-                out_dict["DrumHits"].append(drum_hit)
-print(tempo_index)
-print("Ticks Per Beat " + str(mid.ticks_per_beat) + ", Tempo " + str(tempo) + ", BPM " + '%.2f'%tempo2bpm(tempo))
-print("Midi File Length " + str(mid.length))
-print("Our totaled file length " + str(longest_time))
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        if (track.name == "PART DRUMS"):
+            is_rhythm_game_midi = True
+            class_to_default_notes = rhythm_midi_note_to_drums
+            track_to_convert = track
 
+    if drum_set_dict is None:
+        for drum_class in class_to_default_notes:
+            print(drum_class)
+            note_to_drums_map[class_to_default_notes[drum_class]]= [str(drum_class)+"Default"]
+    else:
+        for drum_obj in drum_set_dict["DrumLayout"]:
+            if drum_obj["Class"] in class_to_default_notes:
+                note_to_drums_map[class_to_default_notes[drum_obj["Class"]]] = [str(drum_obj["DrumName"])]
+        out_dict["DrumLayout"] = drum_set_dict["DrumLayout"] 
 
-with open(script_dir + '/../resources/base/rlrr_files/' + midi_file_name.split('/')[-1].split('.')[0] + '_converted.json', 'w') as outfile:  
-    json.dump(out_dict, outfile, indent=4)
+    print(note_to_drums_map)
+    tempo_total_ticks = 0
+    tempo_index = 0
+
+    for i, track in enumerate(mid.tracks): 
+        for msg in track:
+            if msg.is_meta:
+                if msg.type == "set_tempo":
+                    tempo = msg.tempo
+                    tempo_total_ticks += msg.time
+                    tempo_events.append((tempo_total_ticks, msg.tempo))
+
+    print("Tempo Changes: " + str(tempo_events))
+    for msg in track_to_convert:
+        total_ticks += msg.time
+        while (tempo_index+1 < len(tempo_events)) and (total_ticks > tempo_events[tempo_index+1][0]):
+            tempo_index += 1
+        tempo = tempo_events[tempo_index][1]
+        # total_time = total_ticks * (tempo / (1000000.0 * mid.ticks_per_beat))
+        total_time = total_time + mido.tick2second(msg.time, mid.ticks_per_beat, tempo)
+        if(total_time > longest_time):
+            longest_time = total_time
+        if not msg.is_meta:
+            if msg.type == "note_on":
+                drum_name = "Test"
+                note = msg.note
+                # if is_rhythm_game_midi:
+                    # note = msg.note 
+                #ignore velocity 0 notes here? Seem to be getting a lot of these in the rhythm game midis, almost like note off events are showing up here
+                if note in note_to_drums_map and msg.velocity > 0:
+                    drum_name = note_to_drums_map[note][0]
+                    drum_hit = {"DrumName" : drum_name, "Vel" : msg.velocity, "Loc": 0, "Time": '%.4f'%total_time}
+                    out_dict["DrumHits"].append(drum_hit)
+    print(tempo_index)
+    print("Ticks Per Beat " + str(mid.ticks_per_beat) + ", Tempo " + str(tempo) + ", BPM " + '%.2f'%tempo2bpm(tempo))
+    print("Midi File Length " + str(mid.length))
+    print("Our totaled file length " + str(longest_time))
+
+def convert_to_rlrr():
+    print("Converting to rlrr")
+    with open(script_dir + '/../resources/base/rlrr_files/' + midi_file_name.split('/')[-1].split('.')[0] + '_converted.json', 'w') as outfile:  
+        json.dump(out_dict, outfile, indent=4)
+    print("Conversion done!")
  
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -166,18 +171,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.selectMidiButton.clicked.connect(self.select_midi_clicked)
         self.ui.selectDrumSetButton.clicked.connect(self.select_drum_set_clicked)
+        self.ui.selectAudioFileButton.clicked.connect(self.select_audio_file_clicked)
+        self.ui.convertButton.clicked.connect(self.convert_clicked)
+
+        analyze_drum_set('')
+        # self.midi_converter = MidiConverter()
 
     def select_midi_clicked(self):
         midi_file = QFileDialog.getOpenFileName(self, ("Select Midi File"), ".", ("Midi Files (*.mid *.midi)"))
         print(midi_file)
+        analyze_midi_file()
         self.ui.midiLabel.setText("Selected Midi File: " + midi_file[0].split('/')[-1])
 
     def select_drum_set_clicked(self):
         drum_set_file = QFileDialog.getOpenFileName(self, ("Select Drum Set File"), ".", ("PD Drum Set Files (*.rlrr)"))
         print(drum_set_file)
+        analyze_drum_set(drum_set_file)
         self.ui.selectedSetLabel.setText("Selected Drum Set: " + drum_set_file[0].split('/')[-1])
 
-# app = QtWidgets.QApplication([])
-# application = MainWindow()
-# application.show()
-# sys.exit(app.exec())
+    def select_audio_file_clicked(self):
+        audio_file = QFileDialog.getOpenFileName(self, ("Select Audio File"), ".", ("Audio Files (*.mp3 *.wav *.ogg)"))
+        print(audio_file)
+        self.ui.selectedAudioFileLabel.setText("Selected Audio File: " + drum_set_file[0].split('/')[-1])
+
+    def convert_clicked(self):
+        convert_to_rlrr()
