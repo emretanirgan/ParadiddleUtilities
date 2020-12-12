dadec8504bc7b49a1ad84cd6a946f463dadc033d
Emre Tanirgan <tanirganemre@gmail.com>
2020-12-11 23:18:00 -0800

    Detect length from midi file, updated default midi mapping yaml paths

--- src/main/python/midiconvert.py
+++ src/main/python/midiconvert.py
@@ -22,13 +22,9 @@ out_dict = {
 
 difficulty_names = ['Easy', 'Medium', 'Hard', 'Expert']
 difficulty = 'Easy'
-# TODO set up GUI for input midi, input drum set, output recording file names with file dialogs for each
 
-# TODO user should specify drum set file path, if not use default set file
-# If this file is not fed in, then use default
-# drum_set_file = '/Users/etanirgan/test.json'
 script_dir = os.path.dirname(os.path.realpath(__file__))
-# drum_set_file = ''
+resources_dir = ''
 drum_set_file = os.path.join(script_dir,'resources','base','drum_sets','defaultset.rlrr')
 drum_set_dict = None
 midi_file_name = ''
@@ -64,92 +60,8 @@ author_name = ''
 recording_description = ''
 calibration_offset = 0.0
 
-# TODO put in actual default notes
-# TODO support for mallet instruments, or instruments that can have a lot of midi notes. might have to specify a range of notes
-# for some of them like '48-102' etc
 # TODO support for drum types with more than 1 hit zone - can map from midi note to
 # a tuple of (drum class, location) instead (or just drum class if we want to use a default location value of 0)
-class_to_default_notes = {}
-rlrr_default_notes = {
-    "BP_HiHat_C"    : 60,
-    "BP_Snare_C"    : 57,
-    "BP_Kick_C"     : 56,
-    "BP_Crash15_C"  : 42,
-    "BP_Crash17_C"  : 43,
-    "BP_FloorTom_C" : 44,
-    "BP_Ride17_C"   : 45,
-    "BP_Ride20_C"   : 47,
-    "BP_Tom1_C"     : 48,
-    "BP_Tom2_C"     : 49
-}
-
-# pdtracks_notes = {
-#     "BP_HiHat_C"    : 45,
-#     "BP_Snare_C"    : 26,
-#     "BP_Kick_C"     : 24,
-#     "BP_Crash15_C"  : 62,
-#     "BP_Crash17_C"  : 67,
-#     "BP_FloorTom_C" : 39,
-#     "BP_Ride17_C"   : 75,
-#     "BP_Tom1_C"     : 35,
-#     "BP_Tom2_C"     : 37
-# }
-
-pdtracks_notes = {
-    24 : {"drum": "BP_Kick_C"},
-    26 : {"drum": "BP_Snare_C"},
-    30 : {"drum": "BP_Snare_C"},
-    43 : {"drum": "BP_HiHat_C"},
-    45 : {"drum": "BP_HiHat_C"},
-    48 : {"drum": "BP_HiHat_C"},
-    35 : {"drum": "BP_Tom1_C"},
-    37 : {"drum": "BP_Tom2_C"},
-    39 : {"drum": "BP_FloorTom_C"},
-    62 : {"drum": "BP_Crash15_C"},
-    67 : {"drum": "BP_Crash17_C"},
-    75 : {"drum": "BP_Ride17_C"}
-}
-
-pdtracks_notes_easy = {
-    24 : {"drum": "BP_Kick_C"},
-    26 : {"drum": "BP_Snare_C"},
-    30 : {"drum": "BP_Snare_C"},
-    43 : {"drum": "BP_HiHat_C"},
-    45 : {"drum": "BP_HiHat_C"},
-    48 : {"drum": "BP_HiHat_C"},
-    35 : {"drum": "BP_FloorTom_C"},
-    37 : {"drum": "BP_FloorTom_C"},
-    39 : {"drum": "BP_FloorTom_C"},
-    62 : {"drum": "BP_Crash15_C"},
-    67 : {"drum": "BP_Crash17_C"},
-    75 : {"drum": "BP_Crash15_C"}
-}
-
-# red, (Snare Drum)
-# yellow, (Hi-Hat)
-# blue, (Tom-Tom)
-# green (Crash Cymbols).
-# The kick pedal is colored orange (Bass Drum).
-# 60: guitar note GREEN, easy (C) 
-# 61: guitar note RED, easy (C#) 
-# 62: guitar note YELLOW, easy (D) 
-# 63: guitar note BLUE, easy (D#) 
-# 64: guitar note ORANGE, easy (E) 
-# 67: star power group, easy (G) 
-# 69: player 1 section, easy (A) 
-# 70: player 2 section, easy (A#) 
-# 72: guitar note GREEN, medium (C) 
-# 73: guitar note RED, medium (C#) 
-# 74: guitar note YELLOW, medium (D) 
-# 75: guitar note BLUE, medium (D#) 
-# 76: guitar note ORANGE, medium (E) and so on (60 + difficulty*12)
-rhythm_midi_note_to_drums = {
-    "BP_Kick_C"     : 96,
-    "BP_Snare_C"    : 97,
-    "BP_HiHat_C"    : 98,
-    "BP_Tom1_C"     : 99,
-    "BP_Crash15_C"  : 100
-}
 
 def analyze_drum_set(drum_set_filename):
     global drum_set_dict
@@ -176,12 +88,7 @@ def get_default_midi_track():
 
     print('Midi file type: ' + str(mid.type))
     is_rhythm_game_midi = False
-    # class_to_default_notes = rlrr_default_notes
-    # class_to_default_notes = pdtracks_notes
-    # if track_index == -1:
     convert_track_index = 0 if mid.type == 0 else (1 if len(mid.tracks) > 1 else 0)
-    # else:
-        # convert_track_index = track_index
 
     for i, track in enumerate(mid.tracks):
         print('Track {}: {}'.format(i, track.name))
@@ -195,11 +102,20 @@ def get_default_midi_track():
 
 def analyze_midi_file():
     global out_dict, convert_track_index
+    global length
     out_dict["version"] = 0.6
     out_dict["instruments"] = []
     out_dict["events"] = []
     out_dict["bpmEvents"] = []
     mid = MidiFile(midi_file)
+
+    try:
+        # print("Mid length: " + str(mid.length))
+        midi_len = mid.length
+        if length < midi_len:
+            length = midi_len
+    except ValueError:
+        print("Invalid midi file type to get length")
 
     tempo = 500000
     #list of tuples in the form of (total_ticks, total_seconds, new_tempo)
@@ -207,37 +123,12 @@ def analyze_midi_file():
     total_time = 0.0
     total_ticks = 0.0
     longest_time = 0.0
-
-    #TODO need default midi mappings for rhythm game midi format - get difficulties, map from those midi notes
-    #https://rockband.scorehero.com/forum/viewtopic.php?t=1711
-    #https://www.scorehero.com/forum/viewtopic.php?t=1179
-    #TODO convert from .chart? eventually
-
-    # print('Midi file type: ' + str(mid.type))
-    # is_rhythm_game_midi = False
-    # # class_to_default_notes = rlrr_default_notes
-    # # class_to_default_notes = pdtracks_notes
-    # if track_index == -1:
-    #     convert_track_index = 0 if mid.type == 0 else (1 if len(mid.tracks) > 1 else 0)
-    # else:
-    #     convert_track_index = track_index
         
     # note_to_drums_map = pdtracks_notes
     diff_index = difficulty_names.index(difficulty)
     # fall back to highest difficulty map if our difficulty isn't in the map
     note_map = copy.deepcopy(note_to_drum_maps[min(len(note_to_drum_maps)-1, diff_index)])
-    # note_to_drums_map = copy.deepcopy(pdtracks_notes_easy)
     track_to_convert = mid.tracks[convert_track_index]
-
-    # for i, track in enumerate(mid.tracks):
-    #     print('Track {}: {}'.format(i, track.name))
-    #     midi_track_names.append(track.name)
-    #     if (track.name == "PART DRUMS"):
-    #         is_rhythm_game_midi = True
-    #         class_to_default_notes = rhythm_midi_note_to_drums
-    #         track_to_convert = track
-    #         convert_track_index = i
-    #         print("PART DRUMS: " + str(i))
 
     print("Kit layout again: " + str(drum_set_dict["instruments"]))
     # if drum_set_dict is None:
@@ -313,6 +204,7 @@ def analyze_midi_file():
     # print(out_dict)
 
 def create_midi_map(midi_yaml):
+    global difficulty_names
     '''Construct dicts for each difficulty that
     are in the form [note] : {'drum': [drum_class]}
     This makes lookups easier later on when we analyze the midi file.'''
@@ -322,7 +214,7 @@ def create_midi_map(midi_yaml):
         note_map = {}
         print(midi_yaml[diff.lower()])
         diff_map = midi_yaml[diff.lower()]
-        if len(diff_map) == 0:
+        if not diff_map or len(diff_map) == 0:
             continue
         for drum in diff_map:
             for note in diff_map[drum]:
@@ -407,7 +299,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setOutputButton.clicked.connect(self.set_output_clicked)
         # self.ui.calibrationSpinBox.valueChanged.connect(self.calibration_offset_changed)
         self.ui.selectCoverImageButton.clicked.connect(self.select_cover_image_clicked)
-        # self.ui.midiTrackComboBox.currentIndexChanged.connect(self.midi_track_index_changed)
+        self.ui.midiTrackComboBox.currentIndexChanged.connect(self.midi_track_index_changed)
         self.ui.difficultyComboBox.currentTextChanged.connect(self.difficulty_text_changed)
         self.lastOpenFolder = "."
 		
@@ -415,12 +307,17 @@ class MainWindow(QtWidgets.QMainWindow):
         analyze_drum_set(default_set)
         print(os.path.join(os.path.join(os.path.dirname(default_set), '..'), 'rlrr_files'))
         global output_rlrr_dir
-        output_rlrr_dir = os.path.join(os.path.join(os.path.dirname(default_set), '..'), 'rlrr_files')
-        # output_rlrr_dir = default_set.split('/')
-        # self.midi_converter = MidiConverter()
-    
+        global resources_dir
+        resources_dir = os.path.join(os.path.dirname(default_set), '..')
+        output_rlrr_dir = os.path.join(resources_dir, 'rlrr_files')
+        midi_yaml = os.path.join(resources_dir, 'midi_maps', 'pdtracks_mapping.yaml')
+        self.lastOpenFolder = resources_dir
+        with open(midi_yaml) as file:
+            midi_yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
+            create_midi_map(midi_yaml_dict)
+            self.ui.midiMappingLineEdit.setText(os.path.basename(midi_yaml))
+
     def difficulty_text_changed(self, text):
-        # print("New difficulty: " + text)
         global difficulty
         difficulty = text
 
@@ -429,7 +326,7 @@ class MainWindow(QtWidgets.QMainWindow):
         global midi_track_names
         global convert_track_index
         midi_file = QFileDialog.getOpenFileName(self, ("Select Midi File"), self.lastOpenFolder, ("Midi Files (*.mid *.midi)"))[0]
-        print(midi_file)
+        # print(midi_file)
         # analyze_midi_file()
         get_default_midi_track()
         self.lastOpenFolder = midi_file.rsplit('/', 1)[0]
@@ -443,7 +340,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.midiTrackComboBox.setCurrentIndex(convert_track_index)
 
     def select_midi_map_clicked(self):
-        midi_yaml = QFileDialog.getOpenFileName(self, ("Select Midi File"), self.lastOpenFolder, ("Midi Map (*.yaml)"))[0]
+        midi_yaml = QFileDialog.getOpenFileName(self, ("Select Midi File"), self.lastOpenFolder, ("Midi Map (*.yaml *yml)"))[0]
         with open(midi_yaml) as file:
             midi_yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
             create_midi_map(midi_yaml_dict)
@@ -456,7 +353,7 @@ class MainWindow(QtWidgets.QMainWindow):
         output_rlrr_dir = output_folder
 
     def midi_track_index_changed(self, index):
-        print("new index: " + str(index))
+        # print("new index: " + str(index))
         global convert_track_index
         convert_track_index = index
 
