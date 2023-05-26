c1895b81553967d450be2c1a01da39b3dc38d1fa
bevinart <ebpisarsky@gmail.com>
2023-05-26 13:09:27 -0700

    Complete overhaul of batch run file

---
+++ PDUtil.bat
@@ -0,0 +1,49 @@
+@ECHO OFF
+start /b /wait cmd /c env\Scripts\activate.bat || goto :init
+goto :run
+
+:init
+    :: Ensures all dependencies are met before continuing application
+    call :check_deps
+    call :create_env
+:run
+    :: Run ParadiddleUtilities application
+    python -m fbs run
+    exit /b 0
+
+:create_env
+    :: Creates a new virtual environment
+    python -m virtualenv env 2>nul 
+    call :activate_env
+    :: Installs all packages listed in 'requirements.txt'
+    python -m pip install -r requirements.txt
+    exit /b 0
+
+:activate_env
+    :: Activates new environment
+    start /b /wait cmd /c env\Scripts\activate.bat
+    exit /b 0
+
+:check_deps
+    :: Checks to see if python is installed and easily accessible through $PATH
+    where /q python || (
+        echo Python 3 must be installed to run this application
+        goto :error
+    )
+    :: Checks to see if pip is installed with python
+    python -m pip --version || (
+        echo pip could not be found or is not installed.
+        goto :error
+    )
+    :: Checks if virtualenv is installed
+    python -m virtualenv --version || (
+        echo Installing 'virtualenv'
+        :: IF 'virtualenv' isn't found, install it
+        python -m pip install virtualenv || goto :error
+    )
+    exit /b 0
+
+:error
+    echo.
+    echo Failed with code %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
