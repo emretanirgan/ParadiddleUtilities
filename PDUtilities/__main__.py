5a8ffe6a93b58d245b9612949ef460f1509ebc32
bevinart <ebpisarsky@gmail.com>
2023-12-11 17:28:45 +0000

    :(

--- PDUtilities/__main__.py
+++ PDUtilities/__main__.py
@@ -4,7 +4,7 @@ from pd_gui import PD_GUI
 
 if __name__ == '__main__':
     # Sets primary window for the application
-    print("I have aids")
+    print("I havent seen my wife and kids in over 2 years")
     app = QtWidgets.QApplication(sys.argv)
     window = PD_GUI()
     app.exec_()
