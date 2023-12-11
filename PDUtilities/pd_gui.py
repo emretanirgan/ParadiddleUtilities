e3cec1eeeb21df7ba814629013eb90a11063d570
bevinart <ebpisarsky@gmail.com>
2023-12-11 17:25:59 +0000

    balls

--- PDUtilities/pd_gui.py
+++ PDUtilities/pd_gui.py
@@ -212,7 +212,7 @@ class PD_GUI(QtWidgets.QMainWindow):
             if (songName == ""):
                 self._show_error("This chart needs a song name")
                 return
-            outputDir = os.path.join(Path(self.outputTextBox.toPlainText()), rlrr.metadata.artist + ' - ' + rlrr.metadata.title)
+            outputDir = self.outputTextBox.toPlainText() + '/' + rlrr.metadata.artist + ' - ' + rlrr.metadata.title
             os.makedirs(outputDir, exist_ok=True)
             if (outputDir == ""):
                 self._show_error("Output directory not set")
